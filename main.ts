2cabf5baed62ea3e24a85a0068c32270e59bba6d
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-28 11:54:13 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 basic.showIcon(IconNames.Heart)
 basic.pause(1000)
+basic.showString("Hello!")
 cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ffff)
-cuteBot.forward()
 basic.pause(1000)
 cuteBot.forward()
-basic.pause(1000)
-cuteBot.stopcar()
 basic.forever(function () {
     basic.pause(1000)
     basic.showLeds(`
