2d5204e4f0a6122865e804bcee079d2d7ca35d12
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-28 11:37:17 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+let prava_led_dole: neopixel.Strip = null
 let leva_led_dole: neopixel.Strip = null
 basic.showIcon(IconNames.Heart)
 basic.pause(1000)
 cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ffff)
 leva_led_dole.showColor(neopixel.colors(NeoPixelColors.Blue))
+prava_led_dole.showColor(neopixel.colors(NeoPixelColors.Blue))
+leva_led_dole = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
+prava_led_dole = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
 basic.forever(function () {
     basic.pause(1000)
     basic.showLeds(`
